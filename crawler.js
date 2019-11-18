526a54e72754e02767bce5f9695c8667017ba597
kimseunggyu <dev.seunggyu@gmail.com>
2019-11-18 11:52:07 +0900

    [refactor] html 요청 재사용성을 위해 코드 수정
    - html 을 요청하는 기능을 재사용하기 위해 매개변수를 추가해 재사용성 되기 위해 코드 수정
    - 재사용하기 위한 hostname 으로 해당 uri 접근하는 객체 생성
    - jsdocs 추가

--- crawler.js
+++ crawler.js
@@ -1,23 +1,37 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const log = console.log;
 
-const getHtml = async () => {
+const URI = {
+  naver: 'https://www.naver.com/',
+  google: 'https://trends.google.com/trends/trendingsearches/daily?geo=KR',
+  // google: 'https://trends.google.com/trends/trendingsearches/daily?geo=US',
+};
+
+/**
+ * 요청을 통해 html 데이터 얻기
+ * @param {string} hostname 크롤링하고자 하는 사이트 hostname
+ */
+const getHtml = async hostname => {
   try {
-    return await axios.get('https://www.naver.com/');
+    return await axios.get(URI[hostname]);
+    // return await axios.get('https://www.naver.com/');
   } catch (error) {
     console.error(error);
   }
 };
 
-getHtml().then(html => {
+/**
+ * 네이버 실시간 검색어를 크롤링해 rank, keyword object 배열 생성
+ * @param {object} html 네이버 html 코드?
+ */
+const NAVERCrawling = html => {
   let dataList = [];
   const $ = cheerio.load(html.data);
   const $bodyList = $('div.ah_roll ul')
     .children('li.ah_item')
     .children('a');
 
-  $bodyList.each(function(index, element) {
+  $bodyList.each(function(index) {
     dataList[index] = {
       rank: $(this)
         .find('span.ah_r')
@@ -29,4 +43,9 @@ getHtml().then(html => {
   });
 
   console.log(dataList);
-});
+};
+
+// 프로그램 시작
+(function main() {
+  getHtml('naver').then(NAVERCrawling);
+})();
